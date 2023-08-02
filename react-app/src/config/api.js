04f04435fa2c1eca5559264d4e06ad702a82857d
yaldamauj <yalda.isk@gmail.com>
2023-08-02 21:32:45 +0100

    updated

--- react-app/src/config/api.js
+++ react-app/src/config/api.js
@@ -1,2 +1,10 @@
 // export const hostNameUrl = "https://<PROJECT-ID>.<REGION>.r.appspot.com";
+import axios from 'axios';
+
 export const hostNameUrl = "http://localhost:8080/api/v1";
+
+const api = axios.create({
+  baseURL: "https://localhost:8080"
+});
+
+export default api;
