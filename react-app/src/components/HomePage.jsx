3ddf03bb258df4aa492c9af2f8aa04b2a5b0fa8f
yaldamauj <yalda.isk@gmail.com>
2023-08-03 14:51:36 +0100

    template for homepage

--- react-app/src/components/HomePage.jsx
+++ react-app/src/components/HomePage.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+
+
+
 
 const HomePage = () => {
-  return (
-    <div>HomePage test page</div>
-  )
+    return (
+        <>
+        <Navbar className="bg-body-tertiary">
+        <Container>
+            <Navbar.Brand href="#home">Bonds Tracker</Navbar.Brand>
+            <Navbar.Toggle />
+            <Nav variant="tabs" defaultActiveKey="/home">
+        </Nav>
+        </Container>
+        </Navbar>
+
+        <Tabs
+            defaultActiveKey="home"
+            transition={false}
+            id="noanim-tab-example"
+            className="mb-3"
+            >
+            <Tab eventKey="option1" title="option 1">
+                Tab content for option 1
+            </Tab>
+            <Tab eventKey="option2" title="option 2">
+                Tab content for option 2
+            </Tab>
+            <Tab eventKey="option3" title="option 3">
+                Tab content for option 3
+            </Tab>
+            <Tab eventKey="option4" title="option 4">
+                Tab content for option 4
+            </Tab>
+            <Tab eventKey="option5" title="option 5">
+                Tab content for option 5
+            </Tab>
+            </Tabs>
+        </>
+      );
 }
 
 export default HomePage
