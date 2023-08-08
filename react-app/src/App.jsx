5177f357baf0bb98decb7a8e7c642a3789326d3e
yaldamauj <yalda.isk@gmail.com>
2023-08-08 04:10:48 +0100

    added all pages for display

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import React from "react";
-import { Bookusers } from "./components/Bookuser";
 import Homepage from "./components/Homepage";
 import { Allbonds } from './components/Allbonds';
 
